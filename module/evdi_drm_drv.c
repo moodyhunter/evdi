d808b27c5629a3e22cd783edd2b44b15705d31a0
Moody <mooodyhunter@outlook.com>
2022-03-27 23:50:32 +0100

    Sun Mar 27 23:50:32 BST 2022

--- module/evdi_drm_drv.c
+++ module/evdi_drm_drv.c
@@ -102,9 +102,9 @@ void evdi_driver_unload(struct drm_device *dev)
 #ifdef CONFIG_FB
     evdi_fbdev_cleanup(evdi);
 #endif /* CONFIG_FB */
-    //    evdi_modeset_cleanup(dev);
+       //    evdi_modeset_cleanup(dev);
 
-    //    kfree(evdi);
+    kfree(evdi);
 }
 
 int evdi_driver_open(struct drm_device *drm_dev, __always_unused struct drm_file *file)
